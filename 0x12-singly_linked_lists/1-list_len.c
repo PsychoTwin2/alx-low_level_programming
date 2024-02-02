85027d37049ba7fa75a9a56ffb29e26f6e09a88b
Francis <francisapika5@gmail.com>
2024-02-02 04:13:31 -0800

    my files

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

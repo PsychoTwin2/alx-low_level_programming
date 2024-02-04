865e2879ca05a3405fcafdd332b048a9141e8162
Francis Apika <francisapika5@gmail.com>
2024-02-04 01:10:26 +0000

    betty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main: Assign a random number to n each time it is executed
+ *main - Assign a random number to n each time it is executed
  *and print the last digit of the number stored in the variable n
  *Return: 0
  */

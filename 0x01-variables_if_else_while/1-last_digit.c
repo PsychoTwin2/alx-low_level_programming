02e5fd9b13ec7676d1f1b5f9160f607591f43226
Francis <francisapika5@gmail.com>
2024-02-03 16:53:25 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,36 +1,36 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- *main: Assign a random number to  n each time it is executed
- *and print the last digit of the number in  n
- *Return: 0
- */
-int main(void)
-{
-	int m;
-	int x;
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	m = n % 10;
-	printf("Last digit of %d is %d ", n, m);
-	if (x > 5)
-	{
-		printf("and is greater than 5");
-	}
-	if (x == 0)
-	{
-		printf("and is 0");
-	}
-	if (x < 6 && x != 0)
-	{
-		printf("and is less than 6 and not 0");
-	}
-
-	printf("\n");
-
-	return (0);
-}
+ 1 #include <stdlib.h>
+  2 #include <time.h>
+  3 #include <stdio.h>
+  4 /*
+  5  * main --> Assign a random number to n each time it executes
+  6  * and prints the last digit of the stored number in n.
+  7  *
+  8  * Return: 0 (succes)
+  9 */
+ 10 int main(void)
+ 11 {
+ 12         int n;
+ 13         int m;
+ 14
+ 15         srand(time(0));
+ 16         n = rand() - RAND_MAX / 2;
+ 17
+ 18         m = n % 10;
+ 19         printf("Last digit of %d is ", n);
+ 20         if (m > 5)
+ 21           {
+ 22                 printf("%d and is greater than 5", m);
+ 23           }
+ 24         if (m == 0)
+ 25           {
+ 26                 printf("%d and is 0", m);
+ 27           }
+ 28         if (m < 6 && m != 0)
+ 29           {
+ 30                 printf("%d and is less than 6 and not 0", m);
+ 31           }
+ 32
+ 33         printf("\n");
+ 34
+ 35         return (0);
+ 36 }

71cd7a2bab6183877b56d263e5d1f7f371738151
Francis Apika <francisapika5@gmail.com>
2024-02-04 12:26:43 +0000

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - Adds two int
+* @num: first number
+* @num1: second int
+* Return: addition of the two int
+*/
+int add(int num, int num1)
+{
+	return (num + num1);
+}
+
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for ( i = n; i <= 98; i++)
+	{
+		printf("%d, ",i);
+	}
+}

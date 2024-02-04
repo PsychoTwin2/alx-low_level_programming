ce0d532a881b87fbc9b21820c4750ca9c422a712
Francis Apika <francisapika5@gmail.com>
2024-02-04 11:29:57 +0000

    file 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,13 @@ int main(void)
 	char c;
 	int digit;
 
-	for (c = 'a'; c <= 'f'; c++)
-	{
-		putchar(c);
-	}
-
 	for ( digit = 0; digit < 10; digit++)
 	{
 		putchar((digit % 10) + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
